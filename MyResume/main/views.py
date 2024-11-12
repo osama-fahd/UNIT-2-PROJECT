4ef2b7ed625079cd5520d6952837bdf01fa0bd82
osama_fahad <osamaf.net@gmail.com>
2024-11-12 13:27:13 +0300

    Home page

---
+++ MyResume/main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+
+# from .models import Contact
+# from .forms import ContactForm
+
+
+def home_view(request: HttpRequest):
+    return render(request, "main/home.html")
+
+def contact_view(request: HttpRequest):
+    # contact_form = ContactForm()
+    
+    # if request.method == "POST":
+    #     contact_form = ContactForm(request.POST)
+    #     if contact_form.is_valid():
+    #         contact_form.save()
+    #         return redirect('main:home_view')
+    #     else:
+    #         return render(request, "main/contact.html")
+    
+    return render(request, "main/contact.html")
+
+def contact_messages_view(request: HttpRequest):
+    
+    return render(request, "main/contact_messages.html")
